c46a69e2455ca2cd420d1745c4a3d4c9d51b5753
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-30 17:35:13 +0530

    added scroll up button

--- displayData/DisplayData.inc.php
+++ displayData/DisplayData.inc.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
     <link rel="stylesheet" href="DisplayData.css" type="text/css">
     <title>Document</title>
 </head>
@@ -39,6 +40,7 @@
     }
    
 ?>
+    <button class="scrollUp"><i class='fas fa-arrow-alt-circle-up'></i></button>
     
 </body>
 </html>
